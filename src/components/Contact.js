092ec9df80481b3da6e09b96afde3661d46b29fe
abdurrahim220 <abdurrahim5@gmail.com>
2023-06-30 22:01:40 +0600

    Work done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { motion } from 'framer-motion'
+import { } from 'react-icons/bs'
+import { fadeIn } from '../variants'
 
 const Contact = () => {
   return <div className='section' id='contact'>Contact</div>;
--- src/components/Work.js
+++ src/components/Work.js
@@ -2,9 +2,77 @@ import React from 'react';
 import { motion } from 'framer-motion'
 import { } from 'react-icons/bs'
 import { fadeIn } from '../variants'
-import { Link } from 'react-scroll';
+
+
+import img1 from '../assets/portfolio-img1.png'
+import img2 from '../assets/portfolio-img2.png'
+import img3 from '../assets/portfolio-img3.png'
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+
+  return <section className='section' id='work'>
+
+    <div className='container mx-auto'>
+
+      <div className='flex flex-col lg:flex-row gap-x-10'>
+
+        <motion.div variants={fadeIn('right', 0.3)}
+          initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }}  className='flex-1 flex-col flex gap-y-12 mb-10 lg:mb-0'>
+
+          <div>
+            <h3 className='h2 leading-tight text-accent'>My Latest <br /> work</h3>
+            <p className='max-w-sm mb-16'></p>
+            <button className='btn btn-sm'>View All Projects</button>
+          </div>
+
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl:'>
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt='' />
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-300 z-50'><span className='text-gradient'>UI/UX Design</span></div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-600 z-50'>
+              <span className='text-3xl text-white'>
+                Project Title
+              </span>
+            </div>
+          </div>
+
+        </motion.div>
+        
+        <motion.div 
+        variants={fadeIn('left', 0.5)}
+        initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} 
+        
+        className='flex-1 flex flex-col gap-y-10'>
+
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl:'>
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500' src={img2} alt='' />
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-300 z-50'><span className='text-gradient'>UI/UX Design</span></div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-600 z-50'>
+              <span className='text-3xl text-white'>
+                Project Title
+              </span>
+            </div>
+          </div>
+
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl:'>
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500' src={img3} alt='' />
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-300 z-50'><span className='text-gradient'>UI/UX Design</span></div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-600 z-50'>
+              <span className='text-3xl text-white'>
+                Project Title
+              </span>
+            </div>
+          </div>
+
+        </motion.div>
+
+      </div>
+
+
+    </div>
+
+  </section >;
 };
 
 export default Work;
